2e1d2b079206a9796e9d21d398d46fe60115134d
Wil-mtseng <55979402+Wil-mtseng@users.noreply.github.com>
2019-12-12 14:22:12 +0200

    Fixed errors

--- spec/Simple_Calculator_2.spec.js
+++ spec/Simple_Calculator_2.spec.js
@@ -1,6 +1,6 @@
 /*Wilfred Ratala
 Simple_calculator Part 2 spec
-22 November 2019 
+12 December 2019 
 */
 
 const Simple_Calculator = require('../src/Simple_Calculator_2');
@@ -10,47 +10,44 @@ describe("Addition", function() {
     it("should add two numbers", function() {
         expect(calculator.addition(1, 1)).toEqual(2);
     });
-});
 
-
-describe("Addition", function() {
     it("should add two numbers even if they negative", function() {
         expect(calculator.addition(-1, -1)).toEqual(-2);
     });
-});
 
-describe("Addition", function() {
     it("should add two numbers", function() {
-        expect(calculator.addition(4, 5)).toEqual(9);
+        calculator.addition(4, 5);
+        calculator.set_slot(1);
+        expect(calculator.set_slot(1)).toEqual(9);
     });
-});
 
-describe("Addition of many numbers", function() {
+    fit("Make use of the slot number when doing calculations", function() {
+        calculator.set_slot(1);
+        expect(calculator.addition('slots', 8)).toEqual(8);
+    });
+
     it("should add many numbers", function() {
         expect(calculator.addition(1, 2, 3, 4)).toEqual(10);
     });
+
+    it("Make use of the Last() when doing calculations", function() {
+        calculator.addition(1, 2);
+        expect(calculator.addition(calculator.last(), 5)).toEqual(8);
+    });
+
 });
+
 
 describe("multiply", function() {
     it("Should multiply two numbers", function() {
         expect(calculator.multiply(1, 2)).toEqual(2);
     });
-});
 
-describe("multiply ", function() {
     it("Should multiply of lots of numbers", function() {
         expect(calculator.multiply(1, 2, 3, 4)).toEqual(24);
     });
-});
 
-describe("Addition", function() {
-    it("Make use of the Last() when doing calculations", function() {
-        calculator.addition(1, 2)
-        expect(calculator.addition(calculator.last(), 5)).toEqual(8);
-    });
-});
 
-describe("multiply", function() {
     it("Make use of the Last() when doing multiplys calculations", function() {
         calculator.multiply(1, 2, 3, 4)
         expect(calculator.multiply(calculator.last(), 5)).toEqual(120);
--- src/Simple_Calculator_2.js
+++ src/Simple_Calculator_2.js
@@ -1,6 +1,6 @@
 /*Wilfred Ratala
-Simple_calculator Part 2 source
-22 November 2019 */
+Simple Calculator source
+12 December 2019 */
 
 
 module.exports = class Simple_Calculator {
@@ -11,21 +11,53 @@ module.exports = class Simple_Calculator {
 
     //Addition function
     addition(...nums) {
+
+        var sum
         this.sum = 0;
+
         for (var i = 0; i < nums.length; i++) {
-            this.sum += nums[i];
+
+            if (nums[i] == "LAST") {
+
+                sum + this.last()
+
+            } else if (typeof nums[i] == 'string') {
+
+                // If string will return only numbers
+                sum += this.get_slot(nums[i].replace[(/[a-zA-Z]/g, '') - 1]);
+
+            } else {
+
+                this.sum += nums[i];
+            }
         }
         this.Last = this.sum;
+
         return this.sum;
     };
 
     //Multiplication function
     multiply(...nums) {
+
         this.multiplication = 1;
+
         for (var i = 0; i < nums.length; i++) {
-            this.multiplication *= nums[i];
+
+            if (nums[i] == "LAST") {
+                multiplication *= this.last()
+            } else if (nums[i] == 'string') {
+
+                // If string will return only numbers
+                multiplication *= this.get_slot(nums[i].replace[(/[a-zA-Z]/g, '') - 1]);
+
+            } else {
+
+                this.multiplication *= nums[i];
+            }
         }
+
         this.Last = this.multiplication;
+
         return this.multiplication;
     };
 
@@ -37,12 +69,12 @@ module.exports = class Simple_Calculator {
     //Setter function
     set_slot(_my_slot) {
         this.slots[_my_slot] = this.Last;
-        return this.slots[slots_my_slot];
+        return this.slots[_my_slot];
     };
 
     //Getter function
     get_slot(_my_slot) {
-        return this.slots[slots_my_slot]
+        return this.slots[_my_slot - 1]
     };
 
 };
